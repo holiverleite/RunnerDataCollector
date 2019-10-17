1c0e720a7f030a9a1e30e314c456564725ad3363
Haroldo Leite <holiverleite@gmail.com>
2019-10-16 21:56:50 -0300

    - Collected values from gyroscope and acelerometer

--- app/src/main/java/com/holiverleite/runnerdatacollector/MainActivity.kt
+++ app/src/main/java/com/holiverleite/runnerdatacollector/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.holiverleite.runnerdatacollector
 
+import android.content.Context
 import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.SystemClock
 import android.widget.Button
-import android.widget.Chronometer
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
@@ -16,21 +19,29 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
-import java.sql.Timestamp
-import java.time.Instant
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private var myRef: DatabaseReference? = null
     private var timeWhenStopped: Long = 0
     private var currentState: String = "0"
 
+    private var mSensorManager: SensorManager? = null
+    private var mAccelerometer: Sensor? = null
+    private var mGyroscope: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        this.mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        this.mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        this.mGyroscope = mSensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+        this.mSensorManager?.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL)
+        this.mSensorManager?.registerListener(this,mGyroscope,SensorManager.SENSOR_DELAY_NORMAL)
+
 
         val database = FirebaseDatabase.getInstance()
         this.myRef = database.getReference("command")
@@ -56,24 +67,28 @@ class MainActivity : AppCompatActivity() {
 
         this.device1.setOnClickListener {
             this.disableButtons(this.device1)
+            this.showSensorParameters()
             this.device1.setBackgroundColor(Color.GREEN)
             this.chronometer.isVisible = true
         }
 
         this.device2.setOnClickListener {
             this.disableButtons(this.device2)
+            this.showSensorParameters()
             this.device2.setBackgroundColor(Color.GREEN)
             this.chronometer.isVisible = true
         }
 
         this.device3.setOnClickListener {
             this.disableButtons(this.device3)
+            this.showSensorParameters()
             this.device3.setBackgroundColor(Color.GREEN)
             this.chronometer.isVisible = true
         }
 
         this.device4.setOnClickListener {
             this.disableButtons(this.device4)
+            this.showSensorParameters()
             this.device4.setBackgroundColor(Color.GREEN)
             this.chronometer.isVisible = true
         }
@@ -113,5 +128,34 @@ class MainActivity : AppCompatActivity() {
         this.device_master.isEnabled = false
 
         button.isGone = false
+    }
+
+    fun showSensorParameters() {
+        this.eixoXLabel.isVisible = true
+        this.eixoYLabel.isVisible = true
+        this.eixoZLabel.isVisible = true
+        this.aceleXLabel.isVisible = true
+        this.aceleYLabel.isVisible = true
+        this.aceleZLabel.isVisible = true
+    }
+
+
+    // SENSOR IMPLEMENTATIOS
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if (event?.sensor!!.type == Sensor.TYPE_ACCELEROMETER) {
+            this.aceleXLabel.text = "AcelerometroX: " + event.values[0].toString()
+            this.aceleYLabel.text = "AcelerometroY: " + event.values[1].toString()
+            this.aceleZLabel.text = "AcelerometroZ: " + event.values[2].toString()
+        }
+
+        if (event?.sensor!!.type == Sensor.TYPE_GYROSCOPE) {
+            this.eixoXLabel.text = "GyroscopeX: " + event.values[0].toString()
+            this.eixoYLabel.text = "GyroscopeY: " + event.values[1].toString()
+            this.eixoZLabel.text = "GyroscopeZ: " + event.values[2].toString()
+        }
     }
 }
